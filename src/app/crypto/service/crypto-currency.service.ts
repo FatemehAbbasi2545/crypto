4258b2a088c4c73af7d4475321fdfa60497ccfc0
Fatemeh Abbasi <107993341+FatemehAbbasi2545@users.noreply.github.com>
2024-10-20 14:31:02 +0330

    Update crypto-currency.service.ts

--- src/app/crypto/service/crypto-currency.service.ts
+++ src/app/crypto/service/crypto-currency.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@angular/core';
 
 import { Observable, Subject } from 'rxjs';
+import { webSocket } from 'rxjs/webSocket';
 
 import { ApiService } from './api.service';
 import { AssetHistoryResponse, AssetsResponse, CryptoCurrency, CurrencyHistory } from './../model/crypto.interface';
 
 @Injectable()
 export class CryptoCurrencyService { 
-  cryptoCurrencies: Array<CryptoCurrency> = []; 
+  cryptoCurrencies: Array<CryptoCurrency> = [];
 
   currentRowChanged$ = new Subject<string>();
   selectedCurrencyHistory$ = new Subject<Array<CurrencyHistory>>();
+
+  currencyPrices$ = webSocket('wss://ws.coincap.io/prices?assets=ALL');
 
   constructor(private api: ApiService) {}
 
